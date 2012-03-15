f519cedbd369b2279c1733caeaaecdb0cff64b23
Daniel Lopes <dl.lopes@gmail.com>
2012-03-15 14:46:53 +0000

    startup mongod and write to main collection

--- lib/Log/UDP/Daemon/MongoDB.pm
+++ lib/Log/UDP/Daemon/MongoDB.pm
@@ -1,23 +1,36 @@
 package Log::UDP::Daemon::MongoDB;
 
 use Modern::Perl;
-use Moose;
+use MongoDB;
 use Data::Dumper;
 
-has 'db', is => 'ro', isa => 'Any';
+use Moose;
+has 'con', is=>'rw', isa=>'Any';
+has 'db', is => 'rw', isa => 'Any';
 has 'conf', is => 'ro', isa => 'HashRef[Any]';
 
 with 'Log::UDP::Daemon::StorableRole';
 
 sub open {
+	my $self = shift;
+	my $conf = $self->conf;
 	
+	$self->con(MongoDB::Connection->new(
+						host => $conf->{'host'}.':'.$conf->{'port'}
+						));
+	my $db = $conf->{'db'};
+	$self->db($self->con->$db);
 }
 
 sub append {
 	my $self = shift;
 	my $msg = shift;
 	
-	say Dumper($msg);
+	my $collection = $msg->{'collection'} || 'main';
+	delete($msg->{'collection'});
+	
+	my $col = $self->db->$collection;
+	$col->insert($msg)
 }
 
 1;
